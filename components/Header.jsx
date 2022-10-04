652899e5ebf35fb8a2e57b73bf904e2e6967cde7
Vikram Narra <vikramnarra@Vikrams-Air.ht.home>
2022-10-04 15:55:20 -0400

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,63 @@
+
+import React, { useState, useEffect } from 'react';
+
+
+import Link from 'next/link';
+import { getCategories } from '../services';
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+  function setColor() {
+    const color = getComputedStyle(document.documentElement).getPropertyValue('--black')
+    if (color === '#f0e7db') {
+      //Dark Mode
+        // --white: #f0e7db;
+  // --black: #212123;
+  // --lightBox: #f5f1e9;
+  // --darkBox: #303034;
+  // --blue: #84e8c8;
+  // --pink: #f762c0;
+      document.documentElement.style.setProperty('--black', '#212123');
+      document.documentElement.style.setProperty('--white', '#f0e7db');
+      document.documentElement.style.setProperty('--lightBox', '#f5f1e9');
+      document.documentElement.style.setProperty('--darkBox', '#303034');
+
+
+    }
+    else {
+      document.documentElement.style.setProperty('--black', '#f0e7db');
+      document.documentElement.style.setProperty('--white', '#212123');
+      document.documentElement.style.setProperty('--lightBox', '#303034');
+      document.documentElement.style.setProperty('--darkBox', '#f5f1e9');
+    }
+  }
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block py-8">
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl pinkblue">MARA</span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          <button className="md:float-right text-white ml-4 font-bold py-2 px-4 btn hover:border-blue-500 rounded" onClick={() => setColor()}>🌗</button>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category, index) => (
+            <Link key={index} href={`/category/${category.slug}`}><span className="md:float-right dark2 mt-2 align-middle heads ml-4 font-semibold cursor-pointer">{category.name}</span></Link>
+          ))}
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Header;
